a3422b552fa53339baa1ed106f78402014be6b7e
delphine.millot <delfymillot@gmail.com>
2018-04-04 23:36:24 +0200

    desactivation du ticket journee apres 14h

---
+++ web/Public/js/rate.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    $('input.datepicker').change(function (e) {
+        //$('select#louvre_bookingbundle_orderoftickets_ticketType option').val(0).prop('disabled', false);
+
+        var visitDate;
+        visitDate = $(this).val();
+
+        var today = new Date();
+        var a = today.getFullYear();
+        var m = today.getMonth() +1;
+        var j = today.getDate();
+
+        var h = today.getHours();
+
+        today = '0'+j + '-0' + m + '-' + a;
+
+        if((today === visitDate) && (h >= 14)) {
+            $('#louvre_bookingbundle_orderoftickets_ticketType option:eq(0)')
+                .prop('disabled', true);
+            $('#louvre_bookingbundle_orderoftickets_ticketType option:eq(1)')
+                .prop('selected', true);
+
+        }
+    })
+});
