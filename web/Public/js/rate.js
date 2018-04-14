73d8ee86c81057581e27b55c23f78b745dadf0fa
delphine.millot <delfymillot@gmail.com>
2018-04-14 20:28:59 +0200

    finalisation du calcul de prix sur plusieurs tickets et du total de la commande en js

--- web/Public/js/rate.js
+++ web/Public/js/rate.js
@@ -1,76 +1,92 @@
-var birthDate;
-var myInput;
-var day;
-var month;
-var year;
-var price = 0;
-var total = 0;
 
+var mathHelper = {
+  calculateTotal:function(){
+      var total = 0;
+
+      $('input.price').each(
+          function (index, elem) {
+              var price = $(elem).val();
+              if ($.isNumeric(price)) {
+                  total += parseInt(price);
+              }
+          }
+      );
+      $('.total').val(total);
+
+      return total;
+  },
+  formatNumber:function(num){
+      if(num.length == 1){
+          return "0" + num;
+      }
+      return num;
+  }
+};
 
 $('.tickets').on('change', '.myBirth', function (e) {
 
-    var idTicket = e.target.id.split('_').reverse()[0];
-    var idInput = e.target.id.split('_').reverse()[3];
+    var parent = $(e.target).closest(".form-inline");
+    var idParent = "#" + $(parent).attr("id");
+    var inputDayValue = $(idParent + "_day").val();
+    var inputMonthValue = $(idParent + "_month").val();
+    var inputYearValue = $(idParent + "_year").val();
+    var myInput = $(e.target).closest(".myTicket").find("input.price").eq(0);
 
-    myInput = '#louvre_bookingbundle_orderoftickets_tickets_' + idInput + '_price';
+    var date = mathHelper.formatNumber(inputDayValue) + "-"
+        + mathHelper.formatNumber(inputMonthValue)  + "-" + inputYearValue;
 
-    switch (idTicket) {
-        case 'year' :
-            year = e.target.value;
-            break;
-        case 'month' :
-            month = e.target.value;
-            if (month <= 9) {
-                month = '0' + month;
-            }
-            break;
-        case 'day' :
-            day = e.target.value;
-            if (day <= 9) {
-                day = '0' + day;
-            }
-            break;
-    }
-    if (day == null) {
-        day = '01';
-    }
-    if (month == null) {
-        month = '01';
-    }
-
-    birthDate = day + '-' + month + '-' + year;
-
-// * ****************************** req AJAX ********************************
-    if (null != birthDate) {
-        var url = 'http://localhost:8888/Appli-Louvre/web/app_dev.php/louvre_booking/update';
-        $.post(url, {birthDay: birthDate},
-            function (data) {
-                $(myInput).val(data);
-            })
-            .fail('zut');
-    }
-
+    var url = 'http://localhost:8888/Appli-Louvre/web/app_dev.php/louvre_booking/update';
+    $.post(url, {birthDay: date},
+        function (data) {
+            $(myInput).val(data);
+            mathHelper.calculateTotal();
+        })
+        .fail('zut');
 
 });
 
-$('.tickets').on('blur', '.myBirth', function (e) {
-    $('#louvre_bookingbundle_orderoftickets_ticketsQuantity').change(function () {
-        total = 0;
-        price = 0;
-        $('input.total').val(total);
-    });
-    console.log(total);
-    console.log('input.price.val : ' + $(myInput).val());
-    price = $(myInput).val();
-    price = parseInt(price);
-    // console.log(typeof total);
 
-    total += price;
-    console.log('price : ' + price);
-    console.log('total : ' + total);
 
-    $('input.total').val(total);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
