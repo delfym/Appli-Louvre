1c0395fb0a40353eabd74046d3d1279c484806f7
delphine.millot <delfymillot@gmail.com>
2018-04-27 00:58:47 +0200

    ok

---
+++ src/Louvre/BookingBundle/RateChoice/RateChoice.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: delphinemillotpedrero
+ * Date: 31/03/2018
+ * Time: 19:20
+ */
+
+namespace Louvre\BookingBundle\RateChoice;
+
+
+class RateChoice
+{
+
+}
---
+++ tests/LouvreBookingBundle/RateChoice/LouvreRateChoice.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: delphinemillotpedrero
+ * Date: 26/04/2018
+ * Time: 15:43
+ */
+
+namespace Tests\LouvreBookingBundle\RateChoice;
+
+
+class LouvreRateChoice
+{
+
+}
