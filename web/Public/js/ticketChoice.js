f6f5ad04fc550ec842b2fd5f00316bf761473c10
delphine.millot <delfymillot@gmail.com>
2018-04-28 13:55:28 +0200

    problème de format de date sur le setter de ticketDate / enregistrement en bdd

--- web/Public/js/ticketChoice.js
+++ web/Public/js/ticketChoice.js
@@ -1,28 +1,23 @@
+$('input.datepicker').change(function (e) {
 
+    var visitDate;
+    visitDate = $(this).val();
 
-//$(document).ready(function() {
+    var today = new Date();
+    var a = today.getFullYear();
+    var m = today.getMonth() + 1;
+    m = m.toString();
+    var j = today.getDate();
+    j = j.toString();
+    var h = today.getHours();
 
-    $('input.datepicker').change(function (e) {
+    var currentDay = formAdmin.formatNumber(j) + '-' + formAdmin.formatNumber(m) + '-' + a;
 
-        var visitDate;
-        visitDate = $(this).val();
-
-        var today = new Date();
-        var a = today.getFullYear();
-        var m = today.getMonth() +1;
-        m = m.toString();
-        var j = today.getDate();
-        j = j.toString();
-        var h = today.getHours();
-
-        var currentDay = formAdmin.formatNumber(j) + '-' + formAdmin.formatNumber(m) + '-' + a;
-
-        if((currentDay === visitDate) && (h >= 14)) {
-            $('#louvre_bookingbundle_orderoftickets_ticketType option:eq(0)')
-                .prop('disabled', true);
-            $('#louvre_bookingbundle_orderoftickets_ticketType option:eq(1)')
-                .prop('selected', true);
-        }
-  //  })
+    if ((currentDay === visitDate) && (h >= 14)) {
+        $('#louvre_bookingbundle_orderoftickets_ticketType option:eq(0)')
+            .prop('disabled', true);
+        $('#louvre_bookingbundle_orderoftickets_ticketType option:eq(1)')
+            .prop('selected', true);
+    }
 });
 
